17b3369564d1479d14182d59a0454b9aee57f13e
Marcus V. S. Freitas <marcus.freitas@galp.com>
2025-11-25 17:56:57 +0000

    first commit

---
+++ database/db.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"log"
+	"os"
+	"github.com/guilhermeonrails/api-go-gin/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConectaComBancoDeDados() {
+	db_host := os.Getenv("DB_HOST")
+	db_user := os.Getenv("DB_USER")
+	db_pass := os.Getenv("DB_PASSWORD")
+	db_name := os.Getenv("DB_NAME")
+	db_port := os.Getenv("DB_PORT")
+	
+	stringDeConexao := "host=" + db_host + " user=" + db_user + " password=" + db_pass + " dbname="+ db_name + " port=" + db_port + " sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	if err != nil {
+		log.Panic("Erro ao conectar com banco de dados")
+	}
+
+	DB.AutoMigrate(&models.Aluno{})
+}
